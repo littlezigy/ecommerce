4bf4040d94768f65f6c1db96d5c1e2b46a50afd0
Adesuwa T. Dawodu <littlezigy@gmail.com>
2019-12-05 14:49:25 +0100

    Edited nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
 <nav>
-    <a><img src = './images/logo.png'></a>
+    <a><img src = '/images/logo.png'></a>
     <div>
-        <li><a href = 'pages/login.php'>Login</a></li>
-        <li><a href = 'pages/cart.php'>Cart</a></li>
+        <li><a href = '/pages/login.php'>Login</a></li>
+        <li><a href = '/pages/cart.php'>Cart</a></li>
     </div>
 </nav>
