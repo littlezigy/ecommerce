56a9a682f51be3490ec032c8ffa605939af3ecc1
Adesuwa T. Dawodu <littlezigy@gmail.com>
2019-12-05 20:09:40 +0100

    Fixed css reference in header

--- includes/header.php
+++ includes/header.php
@@ -6,6 +6,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title>Zigy Demo Store</title>
-		<link href = '<?= $site_dir ?>/styles/main.css' rel='stylesheet'>
+		<link href = '/styles/main.css' rel='stylesheet'>
 </head>
 <body>
