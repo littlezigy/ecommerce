96d4be99d1ca47ff1a37100081a340392bdedf80
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-02-06 20:11:21 +0100

    mvc bug test on hosting server

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ require __DIR__ . "/models/productlist.model.php";
 include_once("./includes/header.php");
 
 $url = (isset($_SERVER['PATH_INFO'])) ? $_SERVER['PATH_INFO'] : "/";
+echo $_SERVER['PATH_INFO'];
+echo "REQUEST URI" . $_SERVER['REQUEST_URI'];
 //$urlarray =  (isset($_SERVER['PATH_INFO'])) ? explode('/', $_SERVER['PATH_INFO']) : '/';
 $page = new Page();
 
@@ -23,18 +25,18 @@ if($url === '/') {
 	$homecontroller->loadHomePage($productlist);
 	$page->home($productlist);
 } else {
-	if($url === '/login') {
+	if($url === '/login') { /*
 		if(isset($_SESSION['user'])) {
 			var_dump(xdebug_get_declared_vars());
 			echo "Welcome " . $_SESSION['user'];
 			echo "<script>window.location.replace('/')</script>";
 
 			exit();
-		}
+		}*/
 
 		$page->login();
 	} else if ($url == '/dashboard') {
-		require __dir__ . "/controllers/login.php";
+		require __dir__ . "/controllers/login.controller.php";
 		$logincontroller = new Login();
 	} else if($url === '/dashboard/orders') {
 		echo "<!-- User orders -->";
